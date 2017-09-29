957dcee0da064157324b6a2c4a4ce00cb85c78c5
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2017-09-29 13:35:32 +0300

    Add files via upload

--- download-SRA-convert-paired-fastq.R
+++ download-SRA-convert-paired-fastq.R
@@ -6,7 +6,14 @@ files<-basename(sri$download_path) #in variable files, store the names of all th
 for(i in 1:length(files)) download.file(sri$download_path[i], files[i]) #download all the files and save them with their respective sra names
 
 for(f in files) { #for all these files
-	args_str <- paste("--split-3",f,sep=" ") #prepare arguments for console command
+	args_str<-paste("--split-3",f,sep=" ") #prepare arguments for console command
 	system2("fastq-dump", args=args_str) #run console command to convert from .sra filetype to paired .fastq
 }
+
+fileConn<-file("sampleFile.txt", "w") #w to overwrite, this file will be used as input in the next step of alignment and counting
+cat(sprintf("FileName1\tFileName2\tSampleName\n"), file = fileConn) #this specific format is needed 	 
+for(f in files) { #for all these files
+	cat(sprintf("%s_1.fastq\t%s_1.fastq\t%s\n", f, f, f), file = fileConn) #this specific format is needed
+}
+close(fileConn) #closing connection
 
