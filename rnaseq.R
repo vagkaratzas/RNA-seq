559fa17f525a770f482d204d4d01324fe70c99ab
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2018-02-13 23:33:40 +0200

    Add files via upload

--- rnaseq.R
+++ rnaseq.R
@@ -6,13 +6,14 @@ sampleFile1 <- "samples_fastq.txt"
 sampleFile2 <- "samples_bam.txt"
 genomeFile <- "BSgenome.Hsapiens.NCBI.GRCh38"
 proj1 <- qAlign(sampleFile1, genomeFile) #qAlign(sampleFile1, genomeFile, splicedAlignement=TRUE) #using SpliceMap instead of bowtie
-write.table(alignments(proj1)$genome, sampleFile2, sep="/t", row.names=FALSE)
+write.table(alignments(proj1)$genome, sampleFile2, sep="\t", row.names=FALSE)
 proj2 <- qAlign(sampleFile2, genomeFile, paired='no')
 #####
 qQCReport(proj2, "qc_report.pdf") #fastQC equivalent
 
 library(rtracklayer)
 library(GenomicFeatures)
+library(Rsamtools)
 annotFile <- "Homo_sapiens.GRCh38.83.chr.gtf"
 #export(BSgenome.Hsapiens.NCBI.GRCh38::BSgenome.Hsapiens.NCBI.GRCh38, "BSgenome.Hsapiens.NCBI.GRCh38.fasta") #do once
 chrLen <- scanFaIndex("BSgenome.Hsapiens.NCBI.GRCh38.fasta")
@@ -20,4 +21,4 @@ chrominfo <- data.frame(chrom=as.character(seqnames(chrLen)), length=width(chrLe
 txdb <- makeTxDbFromGFF(file=annotFile, format="gtf", chrominfo=chrominfo, dataSource="NCBI", organism="Homo sapiens")
 geneLevels <- qCount(proj2, txdb, reportLevel="gene") #check how txdb is made
 #geneLevels #DEBUG
-write.table(geneLevels, "gene_count.txt", row.names=TRUE, col.names=TRUE, sep="/t")
+write.table(geneLevels, "gene_count.txt", row.names=TRUE, col.names=TRUE, sep="\t")
