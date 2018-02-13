5e6114ab7f26abb4a273bf9aba959be3dfa225d3
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2018-02-13 19:05:06 +0200

    Delete transcriptome_assembly.sh

--- transcriptome_assembly.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in *.bam;
-do 
-	cufflinks $i
-	mv transcripts.gtf $i.transcripts.gtf
-	mv isoforms.fpkm_tracking $i.isoforms.fpkm_tracking
-	mv genes.fpkm_tracking $i.genes.fpkm_tracking
-	mv skipped.gtf $i.skipped.gtf
-done
