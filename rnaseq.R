cf7532a820282bd17615f8331c37a520290bd20b
Evangelos Karatzas <32259775+vagkaratzas@users.noreply.github.com>
2018-03-07 17:00:02 +0200

    Update rnaseq.R

--- rnaseq.R
+++ rnaseq.R
@@ -1,21 +1,20 @@
 #!/usr/bin/env Rscript
-#setwd("C:/Users/vagos/Desktop/Bio/R/rnaseq")
 
 library(QuasR)
 sampleFile1 <- "samples_fastq.txt"
 sampleFile2 <- "samples_bam.txt"
-genomeFile <- "BSgenome.Hsapiens.NCBI.GRCh38"
-proj1 <- qAlign(sampleFile1, genomeFile) #qAlign(sampleFile1, genomeFile, splicedAlignement=TRUE) #using SpliceMap instead of bowtie
+genomeFile <- "BSgenome.Hsapiens.NCBI.GRCh38.fasta" #or BSgenome.Hsapiens.NCBI.GRCh38 to automaticaly download the genome
+proj1 <- qAlign(sampleFile1, genomeFile) #qAlign(sampleFile1, genomeFile, splicedAlignement=TRUE) #uses SpliceMap instead of bowtie
 write.table(alignments(proj1)$genome, sampleFile2, sep="\t", row.names=FALSE)
-proj2 <- qAlign(sampleFile2, genomeFile, paired='no')
+proj2 <- qAlign(sampleFile2, genomeFile, paired='no') #alternative qProject creation method, from bam files
 #####
 qQCReport(proj2, "qc_report.pdf") #fastQC equivalent
 
 library(rtracklayer)
 library(GenomicFeatures)
 library(Rsamtools)
-annotFile <- "Homo_sapiens.GRCh38.83.chr.gtf"
-#export(BSgenome.Hsapiens.NCBI.GRCh38::BSgenome.Hsapiens.NCBI.GRCh38, "BSgenome.Hsapiens.NCBI.GRCh38.fasta") #do once
+annotFile <- "Homo_sapiens.GRCh38.83.chr.gtf" #important: must be the same version as the alignment genome
+#export(BSgenome.Hsapiens.NCBI.GRCh38::BSgenome.Hsapiens.NCBI.GRCh38, "BSgenome.Hsapiens.NCBI.GRCh38.fasta") #do once if you don't already have the .fasta genome
 chrLen <- scanFaIndex("BSgenome.Hsapiens.NCBI.GRCh38.fasta")
 chrominfo <- data.frame(chrom=as.character(seqnames(chrLen)), length=width(chrLen), is_circular=rep(FALSE, length(chrLen)))
 txdb <- makeTxDbFromGFF(file=annotFile, format="gtf", chrominfo=chrominfo, dataSource="NCBI", organism="Homo sapiens")
